6c77f6e214333898fb7006a4eb134c1a4a8c1797
huanph504 <huanph504@gmail.com>
2011-02-05 04:05:40 +0000

    updated

--- SourceCode/HMS_UI/src/com/hms/form/PatientShell.java
+++ SourceCode/HMS_UI/src/com/hms/form/PatientShell.java
@@ -11,13 +11,34 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 
 public class PatientShell extends Shell {
-	private Text text;
-	private Text text_1;
-	private Text text_2;
-	private Text text_3;
-	private Text text_4;
+	private Text txtID;
+	private Text txtName;
+	private Text txtDayOfBirth;
+	private Button btnDayOfBirth;
+	private Button rdoMale;
+	private Button rdoFemale;
+	private Text txtPhoneNumber;
+	private Text txtEmail;
+	private Text txtJob;
+	private Text txtStreet;
+	private Combo cmbWard;
+	private Combo cmbDistrict;
+	private Combo cmbCity;
+	private Text txtAddress;
+	private Button chkSocialInsurance; 
+	private Text txtType;
+	private Text txtFrom;
+	private Button btnFrom;
+	private Text txtTo;
+	private Button btnTo;
+	private Text txtPlace;
+	private Composite composite_1;
+	
+	
 
 	/**
 	 * Launch the application.
@@ -48,82 +69,202 @@ public class PatientShell extends Shell {
 		setImage(SWTResourceManager.getImage(PatientShell.class, "/com/hms/icon/hms-patient-icon.png"));
 		
 		Composite composite = new Composite(this, SWT.BORDER);
-		composite.setBounds(10, 10, 872, 650);
+		composite.setBounds(10, 10, 622, 500);
 		
 		Label lblHTn = new Label(composite, SWT.NONE);
-		lblHTn.setText(Messages.getString("HMS.PatientManagementShell.label.name"));
+		lblHTn.setText("ID");
 		lblHTn.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
 		lblHTn.setBounds(10, 10, 120, 21);
 		
-		text = new Text(composite, SWT.BORDER);
-		text.setBounds(136, 11, 240, 21);
+		txtID = new Text(composite, SWT.BORDER | SWT.READ_ONLY);
+		txtID.setEditable(false);
+		txtID.setBounds(136, 11, 240, 21);
 		
-		Label lblM = new Label(composite, SWT.NONE);
-		lblM.setText(Messages.getString("HMS.PatientManagementShell.label.id"));
-		lblM.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		lblM.setBounds(10, 91, 120, 21);
+		Label lblName = new Label(composite, SWT.NONE);
+		lblName.setText("Name");
+		lblName.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblName.setBounds(10, 37, 120, 21);
 		
-		text_1 = new Text(composite, SWT.BORDER);
-		text_1.setBounds(136, 92, 240, 21);
+		txtName = new Text(composite, SWT.BORDER);
+		txtName.setBounds(136, 37, 240, 21);
+		txtName.setFocus();
 		
-		Combo combo = new Combo(composite, SWT.NONE);
-		combo.setBounds(136, 37, 240, 21);
+		Label lblDayOfBirth = new Label(composite, SWT.NONE);
+		lblDayOfBirth.setText("Day of birth");
+		lblDayOfBirth.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblDayOfBirth.setBounds(10, 64, 120, 21);
 		
-		Label label = new Label(composite, SWT.NONE);
-		label.setText(Messages.getString("HMS.PatientManagementShell.label.city"));
-		label.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		label.setBounds(10, 37, 120, 21);
+		txtDayOfBirth = new Text(composite, SWT.BORDER);
+		txtDayOfBirth.setTextLimit(10);
+		txtDayOfBirth.setFont(SWTResourceManager.getFont("Tahoma", 10, SWT.NORMAL));
+		txtDayOfBirth.setBounds(136, 65, 100, 21);
 		
-		Label lblGiiTnh = new Label(composite, SWT.NONE);
-		lblGiiTnh.setText(Messages.getString("HMS.PatientManagementShell.label.sex"));
-		lblGiiTnh.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		lblGiiTnh.setBounds(10, 118, 120, 21);
+		btnDayOfBirth = new Button(composite, SWT.NONE);
+		btnDayOfBirth.setImage(SWTResourceManager.getImage(PatientShell.class, "/com/hms/icon/hms-calendar-icon.png"));
+		btnDayOfBirth.setBounds(242, 64, 26, 23);
 		
-		Button btnNam = new Button(composite, SWT.RADIO);
-		btnNam.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		btnNam.setBounds(136, 118, 60, 21);
-		btnNam.setText(Messages.getString("HMS.PatientManagementShell.label.male"));
+		Label lblSex = new Label(composite, SWT.NONE);
+		lblSex.setText("Sex");
+		lblSex.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblSex.setBounds(10, 91, 120, 21);
 		
-		Button btnN = new Button(composite, SWT.RADIO);
-		btnN.setText(Messages.getString("HMS.PatientManagementShell.label.female"));
-		btnN.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		btnN.setBounds(202, 118, 78, 21);
+		composite_1 = new Composite(composite, SWT.NONE);
+		composite_1.setBounds(136, 91, 144, 21);
 		
-		Label lblaCh = new Label(composite, SWT.NONE);
-		lblaCh.setText(Messages.getString("HMS.PatientManagementShell.label.address"));
-		lblaCh.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		lblaCh.setBounds(10, 64, 120, 21);
+		rdoMale = new Button(composite_1, SWT.RADIO);
+		rdoMale.setBounds(0, 0, 60, 21);
+		rdoMale.setSelection(true);
+		rdoMale.setText("Male");
+		rdoMale.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
 		
-		text_2 = new Text(composite, SWT.BORDER);
-		text_2.setBounds(136, 64, 240, 21);
+		rdoFemale = new Button(composite_1, SWT.RADIO);
+		rdoFemale.setBounds(66, 0, 78, 21);
+		rdoFemale.setText("Female");
+		rdoFemale.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
 		
-		Label lblSinThoi = new Label(composite, SWT.NONE);
-		lblSinThoi.setText(Messages.getString("HMS.PatientManagementShell.label.phone_number"));
-		lblSinThoi.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		lblSinThoi.setBounds(10, 145, 120, 21);
+		Label lblPhoneNumber = new Label(composite, SWT.NONE);
+		lblPhoneNumber.setText("Phone number");
+		lblPhoneNumber.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblPhoneNumber.setBounds(10, 117, 120, 21);
 		
-		text_3 = new Text(composite, SWT.BORDER);
-		text_3.setBounds(136, 146, 240, 21);
+		txtPhoneNumber = new Text(composite, SWT.BORDER);
+		txtPhoneNumber.setBounds(136, 118, 240, 21);
 		
-		Label lblTinSBnh = new Label(composite, SWT.NONE);
-		lblTinSBnh.setText(Messages.getString("HMS.PatientManagementShell.label.history"));
-		lblTinSBnh.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		lblTinSBnh.setBounds(10, 172, 120, 21);
+		Label lblEmail = new Label(composite, SWT.NONE);
+		lblEmail.setText("Email");
+		lblEmail.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblEmail.setBounds(10, 145, 120, 21);
 		
-		text_4 = new Text(composite, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
-		text_4.setBounds(136, 172, 606, 82);
+		txtEmail = new Text(composite, SWT.BORDER);
+		txtEmail.setBounds(136, 146, 240, 21);
+		
+		Label lblJob = new Label(composite, SWT.NONE);
+		lblJob.setText("Job");
+		lblJob.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblJob.setBounds(10, 172, 120, 21);
+		
+		txtJob = new Text(composite, SWT.BORDER);
+		txtJob.setBounds(136, 173, 240, 21);
+		
+		Label lblStreet = new Label(composite, SWT.NONE);
+		lblStreet.setText("Street No");
+		lblStreet.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblStreet.setBounds(10, 199, 120, 21);
+		
+		txtStreet = new Text(composite, SWT.BORDER);
+		txtStreet.setBounds(136, 200, 240, 21);
+		
+		Label lblWard = new Label(composite, SWT.NONE);
+		lblWard.setText("Ward");
+		lblWard.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblWard.setBounds(10, 227, 120, 21);
+		
+		cmbWard = new Combo(composite, SWT.NONE);
+		cmbWard.setBounds(136, 227, 240, 21);
+		
+		Label lblDistrict = new Label(composite, SWT.NONE);
+		lblDistrict.setText("District");
+		lblDistrict.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblDistrict.setBounds(10, 254, 120, 21);
+		
+		cmbDistrict = new Combo(composite, SWT.NONE);
+		cmbDistrict.setBounds(136, 254, 240, 21);
+		
+		Label lblCity = new Label(composite, SWT.NONE);
+		lblCity.setText("City");
+		lblCity.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblCity.setBounds(10, 281, 120, 21);
+		
+		cmbCity = new Combo(composite, SWT.NONE);
+		cmbCity.setBounds(136, 281, 240, 21);
+		
+		Label lblAddress = new Label(composite, SWT.NONE);
+		lblAddress.setText("Address");
+		lblAddress.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblAddress.setBounds(10, 308, 120, 21);
+		
+		txtAddress = new Text(composite, SWT.BORDER);
+		txtAddress.setEditable(false);
+		txtAddress.setBounds(136, 308, 440, 21);
+		
+		Label lblSocialInsurance = new Label(composite, SWT.NONE);
+		lblSocialInsurance.setText("Social insurance");
+		lblSocialInsurance.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblSocialInsurance.setBounds(10, 335, 120, 21);
+		
+		chkSocialInsurance = new Button(composite, SWT.CHECK);
+		chkSocialInsurance.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.keyCode == 13) {
+					if (chkSocialInsurance.getSelection()) {
+						chkSocialInsurance.setSelection(false);
+					} else {
+						chkSocialInsurance.setSelection(true);
+					}
+				}
+			}
+		});
+		chkSocialInsurance.setBounds(136, 336, 19, 20);
+		
+		Label lblType = new Label(composite, SWT.NONE);
+		lblType.setText("Type");
+		lblType.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblType.setBounds(10, 362, 120, 21);
+		
+		txtType = new Text(composite, SWT.BORDER);
+		txtType.setBounds(136, 363, 240, 21);
+		
+		Label lblFrom = new Label(composite, SWT.NONE);
+		lblFrom.setText("From");
+		lblFrom.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblFrom.setBounds(10, 389, 120, 21);
+		
+		txtFrom = new Text(composite, SWT.BORDER);
+		txtFrom.setTextLimit(10);
+		txtFrom.setFont(SWTResourceManager.getFont("Tahoma", 10, SWT.NORMAL));
+		txtFrom.setBounds(136, 390, 100, 21);
+		
+		btnFrom = new Button(composite, SWT.NONE);
+		btnFrom.setImage(SWTResourceManager.getImage(PatientShell.class, "/com/hms/icon/hms-calendar-icon.png"));
+		btnFrom.setBounds(242, 389, 26, 23);
+		
+		Label lblTo = new Label(composite, SWT.NONE);
+		lblTo.setText("To");
+		lblTo.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblTo.setBounds(289, 391, 45, 21);
+		
+		txtTo = new Text(composite, SWT.BORDER);
+		txtTo.setTextLimit(10);
+		txtTo.setFont(SWTResourceManager.getFont("Tahoma", 10, SWT.NORMAL));
+		txtTo.setBounds(340, 391, 100, 21);
+		
+		btnTo = new Button(composite, SWT.NONE);
+		btnTo.setImage(SWTResourceManager.getImage(PatientShell.class, "/com/hms/icon/hms-calendar-icon.png"));
+		btnTo.setBounds(446, 390, 26, 23);
+		
+		Label lblPlace = new Label(composite, SWT.NONE);
+		lblPlace.setText("Place");
+		lblPlace.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
+		lblPlace.setBounds(10, 416, 120, 21);
+		
+		txtPlace = new Text(composite, SWT.BORDER);
+		txtPlace.setBounds(136, 417, 240, 21);
+		
+		
 		
 		Button btnLu = new Button(composite, SWT.NONE);
 		btnLu.setImage(SWTResourceManager.getImage(PatientShell.class, "/com/hms/icon/hms-save-icon.png"));
 		btnLu.setText(Messages.getString("HMS.PatientManagementShell.button.save"));
 		btnLu.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		btnLu.setBounds(270, 285, 132, 47);
+		btnLu.setBounds(136, 450, 132, 35);
 		
 		Button btnHy = new Button(composite, SWT.NONE);
 		btnHy.setImage(SWTResourceManager.getImage(PatientShell.class, "/com/hms/icon/hms-cancel-icon.png"));
 		btnHy.setText(Messages.getString("HMS.PatientManagementShell.button.cancel"));
 		btnHy.setFont(SWTResourceManager.getFont("Times New Roman", 12, SWT.BOLD));
-		btnHy.setBounds(442, 285, 132, 47);
+		btnHy.setBounds(307, 450, 132, 35);
+		
 		createContents();
 	}
 
@@ -132,7 +273,7 @@ public class PatientShell extends Shell {
 	 */
 	protected void createContents() {
 		setText(Messages.getString("HMS.HistoryShell.title"));
-		setSize(900, 700);
+		setSize(650, 550);
 
 	}
 
